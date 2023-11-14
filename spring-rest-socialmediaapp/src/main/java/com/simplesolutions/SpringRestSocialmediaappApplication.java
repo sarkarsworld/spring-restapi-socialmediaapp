74a6aac96d6da88416c04064458e80c82adf2da1
aloks <sarkarsworld@gmail.com>
2023-11-14 19:38:32 +0530

    Initial project structure

---
+++ spring-rest-socialmediaapp/src/main/java/com/simplesolutions/SpringRestSocialmediaappApplication.java
@@ -0,0 +1,13 @@
+package com.simplesolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSocialmediaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSocialmediaappApplication.class, args);
+	}
+
+}
